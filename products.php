64644d29206ac51af43fba2a22719be1bffbbfbc
hassehulabeck <hansbudandersson@gmail.com>
2020-05-18 08:50:42 +0200

    Added json

--- products.php
+++ products.php
@@ -17,3 +17,21 @@ $products = [
         'price' => 59
     ]
 ];
+
+$jsonProducts = '[
+    {
+        "title": "Jane Eyre",
+        "format": "pocket",
+        "price": 49
+    }
+    {
+        "title": "Semestra i Yorkshire",
+        "format": "inbunden",
+        "price": 199
+    },
+    {
+        "title": "Stolthet & Fördom",
+        "format": "pocket",
+        "price": 59
+    }
+]';
