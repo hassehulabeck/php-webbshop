b23de61e5003e0ab56f31589d423ec6666f3d165
hassehulabeck <hansbudandersson@gmail.com>
2020-05-18 08:31:24 +0200

    First commit

---
+++ products.php
@@ -0,0 +1,19 @@
+<?php
+
+$products = [
+    [
+        'title' => 'Jane Eyre',
+        'format' => 'pocket',
+        'price' => 49
+    ],
+    [
+        'title' => 'Semestra i Yorkshire',
+        'format' => 'inbunden',
+        'price' => 199
+    ],
+    [
+        'title' => 'Stolthet & Fördom',
+        'format' => 'pocket',
+        'price' => 59
+    ]
+];
