a34219227f235ba2fd2200cbbfec705d3bbd4eae
hassehulabeck <hansbudandersson@gmail.com>
2020-05-20 09:10:41 +0200

    Removed json

--- products.php
+++ products.php
@@ -21,21 +21,3 @@ $products = [
 
     ]
 ];
-
-$jsonProducts = '[
-    {
-        "title": "Jane Eyre",
-        "format": "pocket",
-        "price": 49
-    },
-    {
-        "title": "Semestra i Yorkshire",
-        "format": "inbunden",
-        "price": 199
-    },
-    {
-        "title": "Stolthet & Fördom",
-        "format": "pocket",
-        "price": 59
-    }
-]';
